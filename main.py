1e9fd72b5a024723f7f5d6a42842e4e2f31c5be5
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-18 23:27:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -31,26 +31,18 @@ error_printer(s) => "8/22"
 '''
 
 def printer_error(s):
+    #length of the string
     s_lenght=len(s)
     error_length = 0
 
     for x in s:
+        #sort the string by looking for anything passed M
         if x not in "abcdefghijklm":
+            #count errors
             error_length = error_length + 1
         else: 
             pass
+    #create a variable to return it, so we can print it outside of the function
     msg = '{}/{}'.format(error_length,s_lenght)
     return msg
 
-    
-
-s_lenght=len(s)
-error_length = 0
-
-for x in s:
-    if x not in "abcdefghijklm":
-    # another way: if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u': 
-        error_length = error_length + 1
-    else: 
-        pass
-print(error_length,'/',s_lenght)
